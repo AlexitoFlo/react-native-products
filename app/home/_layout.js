52aa8efebbc178aa2e78d424685ccafc33e0c69c
Flowers <floresfernandezalexis05@gmail.com>
2023-10-30 19:14:35 -0600

    fix:

--- app/home/_layout.js
+++ app/home/_layout.js
@@ -1,12 +1,7 @@
 import { Slot } from 'expo-router'
-import { Header } from 'react-native/Libraries/NewAppScreen'
+
 
 export default function HomeLayout() {
-  return(
-    <>
-      <Header />
-      <Slot />
-      <Footer />
-    </>
-  )
+  return
+      <Slot />;
 }
